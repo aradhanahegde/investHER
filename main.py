2c355aaf4b3ff1977f6388a0958005f070bfe18e
Nimisha Sarkar <83987299+nsarkar123@users.noreply.github.com>
2025-02-08 14:12:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from startScreen import StartScreen
 from profileScreen import ProfileScreen
 from narrative import NarrativeScreen
 from stockScenario import stockScreen 
+from learnMore import learnMoreScreen
 
 class MyApp(App):
     def build(self):
@@ -12,6 +13,7 @@ class MyApp(App):
         sm.add_widget(StartScreen(name='start'))
         sm.add_widget(ProfileScreen(name='profile'))
         sm.add_widget(stockScreen(name='stock'))
+        sm.add_widget(learnMoreScreen(name="learn_more"))
         return sm
 
 if __name__ == '__main__':
